672fbbaaebad2cbf062d07cee77a49fd700a89b8
Muhammet Emre Vatan <emre.vatan98@hotmail.com>
2023-10-05 21:43:14 +0300

    script for employee

---
+++ src/main/resources/sc.sql
@@ -0,0 +1,29 @@
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('f2a6186a-6f86-47c5-bb0d-1e0c2b2e1e8a', 'John Doe', 'USA', 'Male', 5000);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('95b2cd83-cd47-4c7d-8d6b-1f8b141c4e3c', 'Alice Smith', 'UK', 'Female', 5500);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('e027a3b7-3fbb-4f98-8d8a-5fb3d1d3a3e9', 'Bob Brown', 'Canada', 'Male', 5700);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('4dce2bce-2d2a-4cec-a853-555507daa4e5', 'Eve White', 'Australia', 'Female', 4800);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('34dc5efb-e0b4-4516-bf6f-5ddc5b4eaf47', 'Charlie Green', 'Germany', 'Male', 6200);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('76c9ed80-7b91-4e12-9e34-5708c7d2c2ed', 'Donna Black', 'France', 'Female', 5300);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('11b2edf1-d2a4-4c5d-a999-77a77d3f4d3a', 'Frank Blue', 'Italy', 'Male', 4900);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('cc8c2d9e-0aca-40ad-b2c1-5e1a2c9e4f9d', 'Grace Red', 'Spain', 'Female', 5400);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('4ea2b2bf-b3d5-44d5-8b7c-4f6e5c4e4e7c', 'Hank Yellow', 'Portugal', 'Male', 6000);
+
+INSERT INTO batch.employee (GUID, NAME, COUNTRY, GENDER, SALARY)
+VALUES ('992b9e5d-0e0a-45b9-8bcc-2c6e2b0e4e3a', 'Ivy Purple', 'Netherlands', 'Female', 5200);
